bed09a2400186c0c93b6d2a5fc97f952ee405590
Harshvardhan Agarwal <agarwalhars10@gmail.com>
2025-11-18 19:54:47 +0100

    running a debug run on alex with 1 GPU

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -17,17 +17,21 @@ def create_dataset(dataset, config):
     
     pretrain_transform = transforms.Compose([                        
             transforms.RandomResizedCrop(config['image_res'],scale=(0.2, 1.0), interpolation=Image.BICUBIC),
-            transforms.RandomHorizontalFlip(),
-            RandomAugment(2,7,isPIL=True,augs=['Identity','AutoContrast','Equalize','Brightness','Sharpness',
-                                              'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
+            # transforms.RandomHorizontalFlip(),
+            RandomAugment(2,7,isPIL=True,augs=[
+                'Identity','AutoContrast','Equalize','Brightness','Sharpness',
+                # 'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate'
+            ]),
             transforms.ToTensor(),
             normalize,
         ])    
     train_transform = transforms.Compose([                        
             transforms.RandomResizedCrop(config['image_res'],scale=(0.5, 1.0), interpolation=Image.BICUBIC),
-            transforms.RandomHorizontalFlip(),
-            RandomAugment(2,7,isPIL=True,augs=['Identity','AutoContrast','Equalize','Brightness','Sharpness',
-                                              'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
+            # transforms.RandomHorizontalFlip(),
+            RandomAugment(2,7,isPIL=True,augs=[
+                'Identity','AutoContrast','Equalize','Brightness','Sharpness',
+                # 'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate'
+            ]),
             transforms.ToTensor(),
             normalize,
         ])  
@@ -67,9 +71,12 @@ def create_dataset(dataset, config):
     elif dataset=='grounding':
         train_transform = transforms.Compose([                        
                 transforms.Resize((config['image_res'],config['image_res']),interpolation=Image.BICUBIC),
-                transforms.RandomHorizontalFlip(),
-                RandomAugment(2,7,isPIL=True,augs=['Identity','AutoContrast','Equalize','Brightness','Sharpness',
-                                                  'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
+                # transforms.RandomHorizontalFlip(),
+                RandomAugment(2,7,isPIL=True,augs=[
+                    'Identity','AutoContrast','Equalize',
+                    'Brightness','Sharpness',
+                    # 'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate'
+                ]),
                 transforms.ToTensor(),
                 normalize,
             ])         
